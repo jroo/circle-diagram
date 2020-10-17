0e70ca8e896435e4604bd3615c113efb318c006b
Joshua Ruihley <ruihley@gmail.com>
2020-10-17 18:16:38 -0400

    move diagram to class

--- sketch.js
+++ sketch.js
@@ -13,65 +13,73 @@ function setup() {
   createCanvas(w, h);
 
   // load data
-  d = loadJSON('data.json', () => {
-
-    cases = d.days[0].cases;
-    potentialEntries = cases * d.potentialModifier;
-    otkGen = d.days[0].otkGen;
-    otkEnter = d.days[0].otkEnter;
-
+  d = loadJSON('data.json', (d) => {
+    console.log(d);
     // initialize diagram
     angle = TWO_PI / 8;
     buffer = 3;
     maxSize = 500;
-    maxRadius = maxSize / 2;
-    maxArea = getArea(maxRadius);
+    diagram = new CircleDiagram(d, angle, buffer, maxSize);
+    diagram.display();
+  });
+}
 
-    modifier = maxArea / cases;
-    r1 = maxRadius;
-    a1 = maxArea;
-    console.log("cases r1 a1 : " + cases, r1, a1);
+function draw() {}
 
-    a2 = potentialEntries * modifier
-    r2 = getRadius(a2);
-    console.log("potentialEntries r2 a2: " + potentialEntries, r2, a2);
+class CircleDiagram {
+  constructor(data, angle, buffer, maxSize) {
+    this.cases = data.days[0].cases;
+    this.potentialEntries = this.cases * data.potentialModifier;
+    this.otkGen = data.days[0].otkGen;
+    this.otkEnter = data.days[0].otkEnter;
+    print("here2");
 
-    a3 = otkGen * modifier;
-    r3 = getRadius(a3);
-    console.log("otkGen r3 a3: " + otkGen, r3, a3);
+    this.maxRadius = maxSize / 2;
+    this.maxArea = getArea(this.maxRadius);
 
-    a4 = otkEnter * modifier;
-    r4 = getRadius(a4);
-    console.log("otkEnter r4 a4: " + otkEnter, r4, a4);
+    this.modifier = this.maxArea / this.cases;
+    this.r1 = this.maxRadius;
+    this.a1 = this.maxArea;
+    console.log("cases r1 a1 : " + this.cases, this.r1, this.a1);
 
+    this.a2 = this.potentialEntries * this.modifier
+    this.r2 = getRadius(this.a2);
+    console.log("potentialEntries r2 a2: " + this.potentialEntries, this.r2, this.a2);
+
+    this.a3 = this.otkGen * this.modifier;
+    this.r3 = getRadius(this.a3);
+    console.log("otkGen r3 a3: " + this.otkGen, this.r3, this.a3);
+
+    this.a4 = this.otkEnter * this.modifier;
+    this.r4 = getRadius(this.a4);
+    console.log("otkEnter r4 a4: " + this.otkEnter, this.r4, this.a4);
+  }
+
+  display() {
     // display diagram
     background(236, 199, 98);
     strokeWeight(0);
 
-    d1xy = w / 2;
+    this.d1xy = w / 2;
     fill(239);
-    circle(d1xy, d1xy, r1 * 2);
+    circle(this.d1xy, this.d1xy, this.r1 * 2);
 
-    d2x = d1xy + (r1 - r2 - buffer) * cos(angle);
-    d2y = d1xy + (r1 - r2 - buffer) * sin(angle);
+    this.d2x = this.d1xy + (this.r1 - this.r2 - buffer) * cos(angle);
+    this.d2y = this.d1xy + (this.r1 - this.r2 - buffer) * sin(angle);
     fill(239);
     stroke(128);
     strokeWeight(1.5);
-    circle(d2x, d2y, r2 * 2);
+    circle(this.d2x, this.d2y, this.r2 * 2);
 
-    d3x = d2x + (r2 - r3 - buffer) * cos(angle);
-    d3y = d2y + (r2 - r3 - buffer) * sin(angle);
+    this.d3x = this.d2x + (this.r2 - this.r3 - buffer) * cos(angle);
+    this.d3y = this.d2y + (this.r2 - this.r3 - buffer) * sin(angle);
     strokeWeight(0);
     fill(186);
-    circle(d3x, d3y, r3 * 2);
+    circle(this.d3x, this.d3y, this.r3 * 2);
 
-    d4x = d3x + (r3 - r4 - buffer) * cos(angle);
-    d4y = d3y + (r3 - r4 - buffer) * sin(angle);
+    this.d4x = this.d3x + (this.r3 - this.r4 - buffer) * cos(angle);
+    this.d4y = this.d3y + (this.r3 - this.r4 - buffer) * sin(angle);
     fill(94);
-    circle(d4x, d4y, r4 * 2);
-
-    });
-
+    circle(this.d4x, this.d4y, this.r4 * 2);
+  }
 }
-
-function draw() {}
