e2caafaaa6428d96c09a66a3724e00f55370e9da
Joshua Ruihley <ruihley@gmail.com>
2020-10-18 21:39:04 -0400

    made more dynamic

--- sketch.js
+++ sketch.js
@@ -32,6 +32,7 @@ class CircleDiagramFrame {
   constructor(frameNum, frameValues, angle, buffer, maxSize) {
 
     this.frameNum = frameNum;
+    this.frameValues = frameValues;
 
     this.maxRadius = maxSize / 2;
     this.maxArea = this.getArea(this.maxRadius);
@@ -44,32 +45,15 @@ class CircleDiagramFrame {
     //initialize circles
     this.radiusList = []
     this.areaList = []
-    for (i = 0; i < frameValues.length; i++) {
+    for (let i = 0; i < frameValues.length; i++) {
       if (i == 0) {
         this.areaList[i] = this.maxArea;
         this.radiusList[i] = this.maxRadius;
       } else {
         this.areaList[i] = frameValues[i] * this.modifier;
-        this.radiusList[i] = this.getRadius(areaList[i]);
+        this.radiusList[i] = this.getRadius(this.areaList[i]);
       }
     }
-
-
-    this.r1 = this.maxRadius;
-    this.a1 = this.maxArea;
-    //console.log("v1 r1 a1 : " + frameValues[0], this.r1, this.a1);
-
-    this.a2 = frameValues[1] * this.modifier
-    this.r2 = this.getRadius(this.a2);
-    //console.log("v2 r2 a2: " + frameValues[1], this.r2, this.a2);
-
-    this.a3 = frameValues[2] * this.modifier;
-    this.r3 = this.getRadius(this.a3);
-    //console.log("v3 r3 a3: " + frameValues[2], this.r3, this.a3);
-
-    this.a4 = frameValues[3] * this.modifier;
-    this.r4 = this.getRadius(this.a4);
-    //console.log("v4 r4 a4: " + frameValues[3], this.r4, this.a4);
   }
 
   display() {
@@ -81,25 +65,21 @@ class CircleDiagramFrame {
     fill(94);
     text('Frame ' + (this.frameNum + 1), 5, 20);
 
-    this.d1xy = w / 2;
-    fill(239);
-    circle(this.d1xy, this.d1xy, this.r1 * 2);
+    //display circles
+    let dx = []
+    let dy = []
 
-    this.d2x = this.d1xy + (this.r1 - this.r2 - buffer) * cos(angle);
-    this.d2y = this.d1xy + (this.r1 - this.r2 - buffer) * sin(angle);
-    fill(220);
-    circle(this.d2x, this.d2y, this.r2 * 2);
-
-    this.d3x = this.d2x + (this.r2 - this.r3 - buffer) * cos(angle);
-    this.d3y = this.d2y + (this.r2 - this.r3 - buffer) * sin(angle);
-    strokeWeight(0);
-    fill(186);
-    circle(this.d3x, this.d3y, this.r3 * 2);
-
-    this.d4x = this.d3x + (this.r3 - this.r4 - buffer) * cos(angle);
-    this.d4y = this.d3y + (this.r3 - this.r4 - buffer) * sin(angle);
-    fill(94);
-    circle(this.d4x, this.d4y, this.r4 * 2);
+    for (let i = 0; i < this.frameValues.length; i++) {
+      fill(239 - (i*40));
+      if (i == 0) {
+        dx[i] = w / 2;
+        dy[i] = w / 2;
+      } else {
+        dx[i] = dx[i - 1] + (this.radiusList[i - 1] - this.radiusList[i] - buffer) * cos(angle);
+        dy[i] = dy[i - 1] + (this.radiusList[i - 1] - this.radiusList[i] - buffer) * sin(angle);
+      }
+      circle(dx[i], dy[i], this.radiusList[i] * 2); 
+    }
   }
 
   getRadius(area) {
